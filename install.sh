41fc8576bf0e1e7e7f25613e29d516df80dd6491
Elie <elielecas00gmail.com>
2024-11-27 16:36:51 +0100

    cron a faire

---
+++ install.sh
@@ -0,0 +1,3 @@
+bash scripts/1_pip_venv.sh
+bash scripts/2_curl_odbc.sh
+bash sudo apt install cron
--- start.sh
+++ start.sh
@@ -1,6 +1,3 @@
-bash scripts/1_pip_venv.sh
-bash scripts/2_curl_odbc.sh
-
 /home/elie/Documents/Code/Briefs/Datalake_bike/venv/bin/python scripts/3_db.py
 /home/elie/Documents/Code/Briefs/Datalake_bike/venv/bin/python scripts/4_downloads.py
 /home/elie/Documents/Code/Briefs/Datalake_bike/venv/bin/python scripts/5_unzip.py
