10a900ea95a2b29b1c4bd0d1b8ef44ce7e724dce
Elie <elielecas00gmail.com>
2024-11-27 16:45:34 +0100

    cron ok

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
+python3 -m venv venv
+source venv/bin/activate
+pip install -r scripts/requirements.txt
+
 /home/elie/Documents/Code/Briefs/Datalake_bike/venv/bin/python scripts/3_db.py
 /home/elie/Documents/Code/Briefs/Datalake_bike/venv/bin/python scripts/4_downloads.py
 /home/elie/Documents/Code/Briefs/Datalake_bike/venv/bin/python scripts/5_unzip.py
